275cad3838b980cafc0032dd05028b3832fcc3f0
riftfern <adriftfern@gmail.com>
2023-02-26 08:38:43 -0500

    added functions

--- Tree.js
+++ Tree.js
@@ -26,7 +26,88 @@ const Tree = (array) => {
 
     return {
         root: (Array.isArray(array)) ? buildTree(toBeSorted) : array,
-    };
+
+        insert(value, root = this.root){
+            if(root === null){
+                root = Node(value);
+                return root;
+            }
+
+            if(value < root.data){
+                root.left = this.insert(value, root.left);
+            }else if(value > root.data){
+                root.right = this.insert(value, root.right);
+            }
+
+            return root;
+        },
+
+        delete(value, root = this.root){
+            if(root === null){
+                return root;
+            }
+
+            if(value < root.data){
+                root.left = this.delete(value, root.left);
+            }else if(value > root.data){
+                root.right = this.delete(value, root.right);
+            }
+
+            else{
+                if(root.left === null){
+                    return root.right;
+                }else if( root.right === null){
+                    return root.left;
+                }
+
+                else{
+                    const minData = function findNextSmallestRightData(root){
+                        let min = root.data;
+                        let newRoot = root;
+    
+                        while(newRoot.left !==null){
+                            min = root.left.data;
+                            newRoot = root.left;
+                        }
+
+                        return min;
+                    }
+                    root.data = minData(root.right);
+
+                    root.right = this.delete(root.data, root.right)
+                }
+            }
+            return root;
+        },
+        
+        find(value, root = this.root){
+            if(root === null || root.data === value){
+                return root;
+            }
+
+            if(value < root.data){
+                return this.find(value, root.left);
+            }
+            return this.find(value, root.right);
+        },
+
+        levelOrder(arr = [], queue = [], root = this.root){
+            if(root = null) return;
+
+            arr.push(root.data);
+
+            queue.push(root.left);
+            queue.push(root.right);
+
+            while(queue.length){
+                const level = queue[0];
+                queue.shift();
+                this.levelOrder(arr, queue, level)
+            }
+
+            return arr;;
+        },
+    }
 }
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
